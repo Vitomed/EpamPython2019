2d29ca4b7c6742f0ddadc03ace27ce53621f936b
Vitomed <viktor.medyankin@mail.ru>
2019-12-14 17:56:57 +0300

    hw9 HTTP  UDP добавлена чистка кеша с пользователями при выходе из чата

--- 09-HTTP/UDP/server.py
+++ 09-HTTP/UDP/server.py
@@ -23,6 +23,11 @@ while not quit:
             index_stop_name = data.decode("utf-8").find(stop_name)
             client_name = data.decode("utf-8")[index_start_name:index_stop_name]
             clients[client_name] = addr
+            print("clients", clients)
+        else:
+            for name, port_value in clients.items():
+                if addr == port_value:
+                    client_name = name
 
         itistime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
         print(f"[{addr[0]}]==[{addr[1]}]==[{itistime}]/", end="")
@@ -32,10 +37,8 @@ while not quit:
 
         pattern = "`\w+`"
         result_pattern_action = re.findall(pattern, data_list_elements[-1])
-        if result_pattern_action:
-            private_client_name = result_pattern_action[0].replace("`", "")
-
         private_client_name = result_pattern_action[0].replace("`", "") if bool(result_pattern_action) else "empty_name"
+
         if private_client_name in clients:
             private_message = f"ЛС {data_list_elements[0]} " + data_list_elements[-1].replace(result_pattern_action[0],
                                                                                               "")
@@ -43,12 +46,19 @@ while not quit:
             print("Успешно отправлено")
 
         elif "List of clients" in data.decode("utf-8"):
-            s.sendto(str([i for i in clients.keys()]).encode("utf_8"), clients[client_name])
-
+            try:
+                s.sendto(str([i for i in clients.keys()]).encode("utf_8"), clients[client_name])
+            except Exception as e:
+                print("Exc List of client", e)
         else:
             for client in addr_list:
                 if addr != client:
                     s.sendto(data, client)
+            if "left chat" in data.decode(("utf-8")):
+                clients.pop(client_name)
+                addr_list.remove(addr)
+                print("after pop", clients)
+                print("after remove", addr_list)
     except:
         print("\n[Server stop]")
         quit = True
