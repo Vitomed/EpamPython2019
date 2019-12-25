ce59edfee7590a99c2d5e11c937eddecbdb4a0c1
Vitomed <viktor.medyankin@mail.ru>
2019-12-25 22:20:28 +0300

    6hw task 2 Qaternions assert

--- 06-advanced-python/hw/task2.py
+++ 06-advanced-python/hw/task2.py
@@ -5,7 +5,7 @@ https://ru.wikipedia.org/wiki/%D0%9A%D0%B2%D0%B0%D1%82%D0%B5%D1%80%D0%BD%D0%B8%D
 - сложение +
 - умножение
 - деление
-- сравнение +
+- сравнение
 - нахождение модуля +
 - строковое представление и repr +
 По желанию:
@@ -24,26 +24,27 @@ class Quaternion:
         self.d = d
 
     def __add__(self, other):
-        if isinstance(other, (int, float)):
+        if isinstance(other, int) or isinstance(other, float):
             new_a = self.a + other
-            new_Q = Quaternion(new_a, self.b, self.c, self.d)
+            new_b = self.b
+            new_c = self.c
+            new_d = self.d
 
         elif isinstance(other, Quaternion):
             new_a = self.a + other.a
             new_b = self.b + other.b
             new_c = self.c + other.c
             new_d = self.d + other.d
-            new_Q = Quaternion(new_a, new_b, new_c, new_d)
 
         else:
-            raise TypeError("Undefined type")
-        return new_Q
+            raise TypeError("Undefined tcpe")
+        return Quaternion(new_a, new_b, new_c, new_d)
 
     def __radd__(self, other):
         return self.__add__(other)
 
     def __iadd__(self, other):
-        if isinstance(other, (int, float)):
+        if isinstance(other, int) or isinstance(other, float):
             self.a += other
 
         elif isinstance(other, Quaternion):
@@ -56,30 +57,50 @@ class Quaternion:
             raise TypeError("Undefined type")
 
     def __sub__(self, other):  # вычитание
-
-        if isinstance(other, (int, float)):
+        if isinstance(other, int) or isinstance(other, float):
             new_a = self.a - other
-            new_Q = Quaternion(new_a, self.b, self.c, self.d)
+            new_b = self.b
+            new_c = self.c
+            new_d = self.d
 
         elif isinstance(other, Quaternion):
             new_a = self.a - other.a
             new_b = self.b - other.b
             new_c = self.c - other.c
             new_d = self.d - other.d
-            new_Q = Quaternion(new_a, new_b, new_c, new_d)
-
         else:
-            raise TypeError("Undefined type")
-        return new_Q
+            raise TypeError("Undefined tcpe")
+        return Quaternion(new_a, new_b, new_c, new_d)
 
     def __rsub__(self, other):
-        pass
+
+        if isinstance(other, int) or isinstance(other, float):
+            new_a = other - self.a
+            new_b = -self.b
+            new_c = -self.c
+            new_d = -self.d
+
+        elif isinstance(other, Quaternion):
+            self.__sub__(other)
+        else:
+            raise TypeError("Undefined type")
+        return Quaternion(new_a, new_b, new_c, new_d)
 
     def __isub__(self, other):
-        pass
+        if isinstance(other, Quaternion):
+            self.a -= other.a
+            self.b -= other.b
+            self.c -= other.c
+            self.d -= other.d
+
+        elif isinstance(other, (int, float)):
+            self.a -= other
+        else:
+            raise TypeError('Undefined object')
+        return self
 
     def __mul__(self, other):  # умножение
-        if isinstance(other, (int, float)):
+        if isinstance(other, int) or isinstance(other, float):
             new_a = self.a * other
             new_b = self.b * other
             new_c = self.c * other
@@ -106,7 +127,7 @@ class Quaternion:
         return self.__mul__(other)
 
     def __imul__(self, other):  # умножение с присваиванием
-        if isinstance(other, (int, float)):
+        if isinstance(other, int) or isinstance(other, float):
             self.a *= other
             self.b *= other
             self.c *= other
@@ -130,7 +151,7 @@ class Quaternion:
         return Quaternion(new_a, new_b, new_c, new_d)
 
     def __truediv__(self, other):  # деление
-        if isinstance(other, (int, float)):
+        if isinstance(other, int) or isinstance(other, float):
             new_a = self.a / other
             new_b = self.b / other
             new_c = self.c / other
@@ -138,19 +159,22 @@ class Quaternion:
 
         elif isinstance(other, Quaternion):
 
-            new_a = (self.a * other.a - self.b * other.b -
-                     self.c * other.c - self.d * other.d)
+            devider = (self.a + self.a + self.b + self.b +
+                       self.c + self.c + self.d + self.d)
 
-            new_b = (self.a * other.b - self.b * other.a +
-                     self.c * other.d - self.d * other.c)
+            new_a = (-self.a * other.b + self.b * other.a -
+                     self.c * other.d + self.d * other.c) / devider
 
-            new_c = (self.a * other.c - self.b * other.d +
-                     self.c * other.a - self.d * other.b)
+            new_b = (self.a * other.b + self.b * other.c +
+                     self.c * other.a - self.d * other.a) / devider
 
-            new_d = (self.a * other.d + self.b * other.c -
-                     self.c * other.b + self.d * other.a)
+            new_c = (-self.a * other.c + self.b * other.d +
+                     self.c * other.a - self.d * other.b) / devider
+
+            new_d = (-self.a * other.d - self.b * other.c +
+                     self.c * other.b + self.d * other.a) / devider
         else:
-            raise TypeError("Undefined obj")
+            raise TypeError("Undefined type")
         return Quaternion(new_a, new_b, new_c, new_d)
 
     def __eq__(self, other):
@@ -169,91 +193,43 @@ class Quaternion:
         return f"Quaternion({self.a}, {self.b}, {self.c}, {self.d})"
 
 
-a = (1, 2, 3, 4)
-b = (1, 2, 3, 4)
-q1 = Quaternion(*a)
-q2 = Quaternion(*b)
-res = q1 + 5
-
-# print(abs(q1))
-# print(abs(q2))
-# print(res)
-print(q1)
-# print(eval(q1.__repr__()))
-q1 += q2
-print(q1)
-
-import math
+def test_add():
+    assert q + p == Quaternion(q.a + p.a, q.b + p.b, q.c + p.c, q.d + p.d)
+    for s in [-3, -2.3, -1.2, -1.0, 0.0, 0, 1.0, 1, 1.2, 2.3, 3]:
+        assert (q + s == Quaternion(q.a + s, q.b, q.c, q.d))
+        assert (s + q == Quaternion(q.a + s, q.b, q.c, q.d))
 
 
-class Quaternion2:
+def test_subtract():
+    assert q - p == Quaternion(q.a - p.a, q.b - p.b, q.c - p.c, q.d - p.d)
+    for s in [-3, -2.3, -1.2, -1.0, 0.0, 0, 1.0, 1, 1.2, 2.3, 3]:
+        assert (q - s == Quaternion(q.a - s, q.b, q.c, q.d))
+        assert (s - q == Quaternion(s - q.a, -q.b, -q.c, -q.d))
 
-    def __init__(self, a, b=0, c=0, d=0):
-        self.real = a
-        self.i = b
-        self.j = c
-        self.k = d
 
-    def __str__(self):
-        return f'{self.real} + {self.i}i + {self.j}j + {self.k}k'
+def test_mul():
+    assert q * 1.0 == q
+    assert q * 1 == q
+    assert 1.0 * q == q
+    assert 1 * q == q
+    assert 0.0 * q == q * 0.0
+    for s in [-3, -2.3, -1.2, -1.0, 0.0, 0, 1.0, 1, 1.2, 2.3, 3]:
+        assert q * s == Quaternion(s * q.a, s * q.b, s * q.c, s * q.d)
+        assert s * q == q * s
 
-    def __repr__(self):
-        return f'Quaternion({self.real}, {self.i}, {self.j}, {self.k})'
 
-    def __add__(self, other):
-        if isinstance(other, (int, float)):
-            return Quaternion2(self.real + other, self.i, self.j, self.k)
-        elif isinstance(other, Quaternion):
-            a = self.real + other.real
-            b = self.i + other.i
-            c = self.j + other.j
-            d = self.k + other.k
-            return Quaternion2(a, b, c, d)
-        else:
-            raise TypeError('This type is not supported')
+def test_divide():
+    assert q / 1.0 == q
+    assert q / 1 == q
+    for s in [-2.3, -1.2, -1.0, 1.0, 1, 1.2, 2.3]:
+        assert q / s == q * (1.0 / s)
 
-    def __mul__(self, other):
-        if isinstance(other, (int, float)):
-            return Quaternion(self.real * other, self.i * other,
-                              self.j * other, self.k * other)
-        elif isinstance(other, Quaternion2):
-            a = (self.real * other.real - self.i * other.i -
-                 self.j * other.j - self.k * self.k)
-            b = (self.real * other.i + self.i * other.real +
-                 self.j * other.k - self.k * other.j)
-            c = (self.real * other.j + self.j * other.real +
-                 self.k * other.i - self.i * other.k)
-            d = (self.real * other.k + self.k * other.real +
-                 self.j * other.j - self.j * other.i)
-            return Quaternion2(a, b, c, d)
-        else:
-            raise ValueError('This type is not supported')
 
-    def norm(self):
-        return math.sqrt(
-            self.real ** 2 + self.i ** 2 + self.j ** 2 + self.k ** 2)
+if __name__ == '__main__':
+    q = Quaternion(10, 2, 7, 1)
+    p = Quaternion(0, 22, 3, 4)
+    test_add()
+    test_subtract()
+    test_mul()
+    test_divide()
 
-    def convert(self):
-        return Quaternion2(self.real, - self.i, -self.j, -self.k) / (
-                self.norm() ** 2)
-
-    def __truediv__(self, other):
-        if isinstance(other, (int, float)):
-            return Quaternion(self.real / other, self.i / other,
-                              self.j / other, self.k / other)
-        elif isinstance(other, Quaternion):
-            return self * other.convert()
-        else:
-            raise TypeError('This type is not supported')
-
-    def __eq__(self, other):
-        return all(
-            [
-                isinstance(self, Quaternion),
-                isinstance(other, Quaternion),
-                self.real == other.real,
-                self.i == other.i,
-                self.j == other.j,
-                self.k == other.k,
-            ]
-        )
