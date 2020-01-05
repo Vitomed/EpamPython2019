f1eb6fd25d920d673f3198db2fb6b51fd6a3d950
Vitomed <viktor.medyankin@mail.ru>
2020-01-06 00:10:40 +0300

    file handler

--- 14-data-persistence/hw/hw.py
+++ 14-data-persistence/hw/hw.py
@@ -1,202 +1,153 @@
 import json
 import pickle
 import redis
-import sqlite3
-# import pymongo
 
-class Saver:
 
-    def __init__(self, obj, protocol, storage):
-        self.obj_ref = obj
+class JsonSerializer:
+
+    @staticmethod
+    def serialize(data):
+        try:
+            serialize_data = json.dumps(data)
+        except TypeError:
+            raise TypeError(f"Object of type {type(data)} is not JSON serializable")
+        else:
+            print("json serialization")
+            return serialize_data
+
+    @staticmethod
+    def deserialize(new_data):
+        if new_data is not None:
+            deserialize_data = json.loads(new_data)
+            print("json deserialization")
+            return deserialize_data
+
+
+class PickleSerializer:
+
+    @staticmethod
+    def serialize(data):
+        try:
+            serialize_data = pickle.dumps(data)
+        except TypeError:
+            raise TypeError(f"Object of type {type(data)} is not PICKLE serializable")
+        else:
+            print("pickle serialization")
+            return serialize_data
+
+    @staticmethod
+    def deserialize(new_data):
+        if new_data is not None:
+            deserialize_data = pickle.loads(new_data)
+            print("pickle deserialization")
+            return deserialize_data
+
+
+class MetaSingetonRedis(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super().__call__(*args, **kwargs)
+        return cls._instance[cls]
+
+
+class RedisHandler(metaclass=MetaSingetonRedis):
+
+    def __init__(self, protocol):
+        self.r = redis.Redis(host="localhost", port=6379)
         self.protocol = protocol
-        self.storage = storage
 
-    def serialize(self):
-        pass
+    def store_data(self, serial_data, key):
+        print("redis")
+        self.r.set(key, serial_data)
 
-    def deserialize(self):
-        pass
-
-
-class Receiver:
-
-    def __init__(self, obj, protocol, storage):
-        self.obj_ref = obj
-        self.protocol = protocol
-        self.storage = storage
-
-    def serialize(self):
-        pass
-
-    def deserialize(self):
-        pass
+    def recive_data(self, key):
+        return self.r.get(key)
 
 
-def save(obj_ref, protocol, storage):
-    print("1", obj_ref)
-    print("2", protocol)
-    print("3", storage)
-    pass
+class FileHandler:
 
-def receive(obj_ref, protocol, storage):
-    pass
+    def __init__(self, proto):
+        self.name = "file"
+        self.protocol = proto
+        print("file")
 
-# data = {
-#     'a': [1, 2.0, 3, 4+6j],
-#     'b': ("character string", b"byte string"),
-#     'c': {None, True, False}
-# }
-#
-# with open('data.pickle', 'wb') as f:
-#     # Pickle the 'data' dictionary using the highest protocol available.
-#     pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-#
-#
-# with open('data.pickle', 'rb') as f:
-#     # The protocol version used is detected automatically, so we do not
-#     # have to specify it.
-#     data = pickle.load(f)
-# print(data)
+    def store_data(self, serial_data, key):
+        operation_mode = "wb" if self.protocol == "pickle" else "w"
+        with open(f"{key}.txt", operation_mode) as write_file:
+            write_file.write(serial_data)
+
+    def recive_data(self, key):
+        operation_mode = "rb" if self.protocol == "pickle" else "r"
+        with open(f"{key}.txt", operation_mode) as read_file:
+            data = read_file.read()
+            return data
 
 
-def serialization_picle(filename, object):
-    name = filename + ".pickle"
-    with open(name, "wb") as file:
-        pickle.dump(object, file, 2)
+protocols = {"json": JsonSerializer, "pickle": PickleSerializer}
+storage_type = {"redis": RedisHandler, "file": FileHandler}
+
+def worker_saver(data, name_serialize, storage_name):
+    key = [key for key in data.keys()][0]
+
+    protocol = protocols[name_serialize]
+    storage = storage_type[storage_name]
+
+    inst_protocol = protocol()
+    ser_obj = inst_protocol.serialize(data)
+
+    inst_storage = storage(name_serialize)
+    inst_storage.store_data(ser_obj, key)
 
 
-def deserialization_picle(filename):
-    name = filename + ".pickle"
-    with open(name, "rb") as file:
-        data = pickle.load(file)
-        return data
+def worker_reciver(data, name_serialize, storage_name):
+    key = [key for key in data.keys()][0]
 
+    protocol = protocols[name_serialize]
+    storage = storage_type[storage_name]
 
-def serialization_json(filename, object):
-    name = filename + ".json"
-    with open(name, "w") as file:
-        json.dump(object, file)
+    inst_protocol = protocol()
+    inst_storage = storage(name_serialize)
 
+    ser_obj = inst_storage.recive_data(key)
+    deser_obj = inst_protocol.deserialize(ser_obj)
 
-def deserialization_json(filename):
-    name = filename + ".json"
-    with open(name, "r") as file:
-        data = json.load(file)
-        return data
+    pprint(deser_obj)
 
+if __name__ == "__main__":
 
-
-def redis_handler(data, status):
-    r = redis.Redis()
-    if status == 1:
-        r.set(hash(data), data)
-        hashes.append(hash(data))
-    else:
-        return r.get(data)
-
-# def mongo_handler(data, status):
-#     client = pymongo.MongoClient(host='localhost', port=27017)
-#     if status == 1:
-#         db = client['test_db']
-#         coll = db['test_coll']
-#         hash_ = coll.insert({'data': data})
-#         hashes.append(hash_)
-#     else:
-#         db = client['test_db']
-#         coll = db['test_coll']
-#         res = coll.find_one(data)
-#         return coll.find_one(res)['data']
-
-def deserialize_json(data):
-    return json.loads(data)
-
-
-def deserialize_pickle(data):
-    return pickle.loads(data)
-
-
-def serialize_json(data):
-    return json.dumps(data)
-
-
-def serialize_pickle(data):
-    return pickle.dumps(data)
-
-
-def set(data, protocol, storage):
-    if protocol == 'json':
-        data = serialize_json(data)
-    elif protocol == 'pickle':
-        data = serialize_pickle(data)
-    storage_links[storage](data, 1)
-
-
-def get(data, protocol, storage):
-    result = storage_links[storage](data, 2)
-    if protocol == 'json':
-        return deserialize_json(result)
-    elif protocol == 'pickle':
-        return deserialize_pickle(result)
-
-
-# storage_links = {'redis': redis_handler, 'mongo': mongo_handler}
-# storage_links = {'redis': redis_handler}
-# hashes = []
-
-# if __name__ == '__main__':
-    # data_ = {"name": "Scott", "website": "stackabuse.com", "from": "Nebraska"}
-    # set(data_, 'json', 'redis')
-    # result = get(hashes[0], 'json', 'redis')
-    # print(result)
-    # set(data_, 'json', 'mongo')
-    # result = get(hashes[1], 'json', 'mongo')
-    # print(result)
-
-# =====================================================
-
-r = redis.Redis()
-
-restaurant_484272 = {
-    "name": "Ravagh",
-    "type": "Persian",
-    "address": {
-        "street": {
-            "line1": "11 E 30th St",
-            "line2": "APT 1",
-        },
-        "city": "New York",
-        "state": "NY",
-        "zip": 10016,
+    key_name = "my_key"
+    obj = {
+        "name": "Ravagh",
+        "type": "Persian",
+        "address": {
+            "street": {
+                "line1": "11 E 30th St",
+                "line2": "APT 1",
+            },
+            "city": "New York",
+            "state": "NY",
+            "zip": 10016,
+        }
     }
-}
 
-from pprint import pprint
+    data = {key_name: obj}
 
-# r.set(484272, json.dumps(restaurant_484272)) # True
-# # pprint(json.loads(r.get(484272)))
-# obj = json.loads(r.get(484272))
-# # obj = pprint(json.loads(r.get(484272)))
-# pprint(obj)
+    print("="*30)
+    worker_saver(data, "json", "redis")
+    worker_reciver(data, "json", "redis")
 
 
-# r.set(484273, pickle.dumps(restaurant_484272)) # True
-# # pprint(json.loads(r.get(484272)))
-# obj = pickle.loads(r.get(484273))
-# # obj = pprint(json.loads(r.get(484272)))
-# pprint(obj)
+    print("="*30)
+    worker_saver(data, "json", "file")
+    worker_reciver(data, "json", "file")
 
-# print(hash(restaurant_484272))
-hash_list = list()
-hash_list.append(abs(hash(str(restaurant_484272))))
+    print("="*30)
+    worker_saver(data, "pickle", "redis")
+    worker_reciver(data, "pickle", "redis")
 
-r.set(hash_list[0], json.dumps(restaurant_484272)) # True
-# pprint(json.loads(r.get(484272)))
-obj = json.loads(r.get(hash_list[0]))
-# obj = pprint(json.loads(r.get(484272)))
-# pprint(obj)
 
-# =====================================================
-
-import os
-
-print(os.environ["REDIS_ENDPOINT"])
+    print("="*30)
+    worker_saver(data, "pickle", "file")
+    worker_reciver(data, "pickle", "file")
