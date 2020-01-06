4230cd382101ea37ab38c75b8e9a686b3ce33dac
Vitomed <viktor.medyankin@mail.ru>
2020-01-06 21:21:28 +0300

    add tests siammeta

--- 07pythonforadvanced/hw/task1.py
+++ 07pythonforadvanced/hw/task1.py
@@ -24,18 +24,17 @@
 # 1
 
 """
-
 import weakref
 import inspect
 
 
 class SingletonCahceMeta(type):
+    pool = weakref.WeakSet()
 
     def __init__(cls, *args, **kwargs):
         super().__init__(*args, **kwargs)
         cls.__cache = weakref.WeakValueDictionary()
         cls.pool = weakref.WeakSet()
-        print("cache", cls.__cache)
 
     def __call__(cls, *args, **kwargs):
 
@@ -45,8 +44,9 @@ class SingletonCahceMeta(type):
             return cls.__cache[key_signature]
         else:
             obj = super().__call__(*args, **kwargs)
-            # cls.connect = cls.connect
             setattr(cls, "connect", cls.connect)
+            # print("cls kwg", kwargs)
+            # cls.pool = SingletonCahceMeta.pool
             cls.pool.add(obj)
             cls.__cache[key_signature] = obj
             return obj
@@ -63,36 +63,18 @@ class SingletonCahceMeta(type):
             raise AttributeError("Object with this attribute set is missing")
 
 
-# class Spam(metaclass=SingletonCahceMeta):
-#     def __init__(self, a, b, c, **kwargs):
-#         self.__dict__ = dict(kwargs)
-#         self.a = a
-#         self.b = b
-#         self.c = c
-
-class Spam(metaclass=SingletonCahceMeta):
-    def __init__(self, **kwargs):
-        self.__dict__ = dict(kwargs)
+class SiamObj(metaclass=SingletonCahceMeta):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.__dict__.update(kwargs)
 
 
-# a = Spam(1, 2, q=12)
-# b = Spam(1, 2, 4)
-# d = Spam(4, 5, 6, w=14)
-# a = Spam(4, 5, 6)
-d = Spam(w=14)
-b = Spam(a=1)
-# print(a is b)
-# print(a is d)
+if __name__ == "__main__":
+    # Please, test pool there
 
-print("dict", d.__dict__)
-print("w", d.w)
-print("connect 1 ", b.connect(w=14).__dict__)
-print("connect 2", b.connect(w=14).w)
-b.connect(w=14).w = 0
-print("d", d.w == 0)
-
-# pool = b.pool
-# print(pool)
-# print(len(pool))
-# del b
-# print(len(pool))
+    u1 = SiamObj(1, 2, 4, a=5)
+    u2 = SiamObj(2, 2, 4, a=5)
+    pool = u1.pool
+    print(len(pool))
+    del u1
+    print(len(pool))
---
+++ 07pythonforadvanced/hw/task3.py
@@ -0,0 +1,38 @@
+"""
+Написать тесты(pytest or unittest) к предыдущим 2 заданиям, запустив которые, я бы смог бы проверить их корректность
+Обязательно проверить всю критическую функциональность
+"""
+import unittest
+from task1 import SiamObj
+
+
+class SiamMetaTest(unittest.TestCase):
+
+    def test_equal(self):
+        self.unit1 = SiamObj("1", 22, c=33)
+        self.unit2 = SiamObj("1", 22, c=33)
+        self.assertEqual(self.unit1, self.unit2)
+
+    def test_not_equal(self):
+        self.unit1 = SiamObj("1", 22, d=33)
+        self.unit2 = SiamObj("1", d=33)
+        self.unit3 = SiamObj(0, "11", p=15)
+        self.assertNotEqual(self.unit1, self.unit3)
+        self.assertNotEqual(self.unit1, self.unit2)
+
+    def test_connec_method(self):
+        self.unit1 = SiamObj(1, 2, q=12)
+        self.unit2 = SiamObj(1, 2, z=4)
+        self.unit3 = SiamObj(1, 2, z=4)
+        self.unit1.connect(1, 2, z=4).z = 100
+        self.assertEqual(self.unit2.z, 100)
+        self.assertEqual(self.unit3.z, 100)
+
+    def test_pool_method(self):
+        self.unit3 = SiamObj(a=1)
+        pool = self.unit3.pool
+        print(len(pool))
+
+
+if __name__ == "__main__":
+    unittest.main()
