f8739d197d8887543a08953dc9d4bfb13fe1a27f
Vitomed <viktor.medyankin@mail.ru>
2020-01-08 04:54:36 +0300

    hw 11 task2 cProfile*, myhashfile*, trace* .txt add

--- 11-programming-and-debugging/hw/task2/profiler.py
+++ 11-programming-and-debugging/hw/task2/profiler.py
@@ -1,39 +1,42 @@
 import os
 import hashlib
+import chardet
+import cProfile
 
 
 def get_hash_file(file):
-    print("get", file)
     with open(file, "rb") as file:
         hash_value = file.read()
-        return hashlib.sha256(hash_value).hexdigest()
+    return hashlib.sha256(hash_value).hexdigest()
 
 
-def main(path, test_hash_value):
+def walker_path(path, searching_hash):
     if not os.path.exists(path):
         return -1
-    print(path)
     try:
         for root, dirs, files in os.walk(path):
-            print("r", root)
-            print("d", dirs)
-            print("f", files)
             for filename in files:
-                print("files", filename)
-                if os.path.isfile(os.path.join(root, filename)):
-                    print("is file", os.path.join(root, filename))
-                    path_to_file = os.path.join(root, filename)
-                    print("hash f", get_hash_file(path_to_file))
-                    hash_file_value = get_hash_file(path_to_file)
-                    if hash_file_value == test_hash_value:
-                        print("True", os.path.abspath(root))
+                path_to_file_ = os.path.join(root, filename)
+                hash_file_value = get_hash_file(path_to_file_)
+                if hash_file_value == searching_hash:
+                    return os.path.abspath(path_to_file_)
 
     except Exception as e:
         print("Exc", e)
 
 
-if __name__ == "__main__":
-    my_path = "/home/vitomed/PycharmProjects/WinterEpamPython2019/11-programming-and-debugging"
-    # print("path", os.path.abspath(my_path))
+def main(path, searching_hash):
+    path_to_file = walker_path(path, searching_hash)
 
-    main(my_path, 12)
+    if path_to_file:
+        print("1",path_to_file)
+        with open(path_to_file, "rb") as file: print(chardet.detect(file.read()))
+    else:
+        print("File not found")
+
+
+if __name__ == "__main__":
+    p = "/home/vitomed/PycharmProjects/WinterEpamPython2019/"
+    s = "37f7cc05f119a614876a98753db519ded14956d2169dbb05c4c89baf45e25c8a"
+    cProfile.run("main(p,s)")
+    # main(p, s)
