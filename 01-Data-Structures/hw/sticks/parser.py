12a3833143d5306f702b94e274635e5386205b4c
Vitomed <viktor.medyankin@mail.ru>
2020-01-09 04:01:17 +0300

    hw1 parser county region counter

--- 01-Data-Structures/hw/sticks/parser.py
+++ 01-Data-Structures/hw/sticks/parser.py
@@ -1,3 +1,4 @@
+import collections
 import re
 
 
@@ -29,16 +30,8 @@ def extraction_data_between_braces(concat_json_file):
     :return: keys, by_elems
     """
     json = concat_json_file.replace("null", '"0"')
-    # json2 = concat_json_file.replace("null", '"0"').replace("[", "").replace("]", "")
-    # print("concat", concat_json_file[:10], concat_json_file[-10:])
-    # print("json2", json2[:10], json2[-10:])
-    # print(json[:1000], end="\n")
-    pattern1 = r"\{(.*?)\}"
-    rows_between_braces = re.findall(pattern1, json)
-    # for i, e in enumerate(rows_between_braces):
-    #     print(e)
-    #     if i == 0:
-    #         break
+    pattern_1 = r"\{(.*?)\}"
+    rows_between_braces = re.findall(pattern_1, json)
 
     keys = [
         'points',
@@ -58,35 +51,39 @@ def extraction_data_between_braces(concat_json_file):
     return keys, rows_between_braces
 
 
-def get_wine_data(keys, datas_btw_braces):
-    """return a list of data framed by braces
+def get_wine_data(list_keys, datas_btw_braces):
+    """return a list of dicts data framed by braces
 
     :param keys: different keys in each row
     :param datas_btw_braces: {data1}, {bata2}... list without {}
     :return: datas for each wine
     """
-
-    wine_data = []
+    list_wine_data = []
     for row in datas_btw_braces:
-
-        pattern2 = r'\: \"(.*?)\"'
-        values = re.findall(pattern2, row)
-        values[0] = int(values[0])
+        pattern_2 = r'\: \"(.*?)\"'
+        list_values = re.findall(pattern_2, row)
 
         if '"price": "0"' not in row:
             pattern3 = r'\"price\"\: (.*?)\,'
-            price = list(map(int, re.findall(pattern3, row)))
-            if price[0] > 0:
-                values.insert(5, price[0])
-        temp_dict = dict(zip(keys, values))
-        wine_data.append(temp_dict)
+            price_of_wine = list(map(int, re.findall(pattern3, row)))  #  find the price value and convert it to class int
+            if price_of_wine[0] > 0:
+                list_values.insert(5, price_of_wine[0])
+        temp_dict = dict(zip(list_keys, list_values))
+        list_wine_data.append(temp_dict)
+    return list_wine_data
 
-    wine_data = sorted(wine_data, key=lambda x: int(x['price']), reverse=True)
 
-    with open('winedata_full.json', 'w', encoding='utf-8') as out:
+def sort_wine_data(list_wine_data):
+    """Function sorts a list of dictionaries
+    by two keys: price and country
+
+    :param list_wine_data: unsorted list dicts of wine data
+    :return: sorted list dicts of wine data
+    """
+    wine_data = sorted(list_wine_data, key=lambda k: (int(k['price']), k["country"]), reverse=True)
+    with open('winedata_full.json', 'w', encoding='utf-8') as full_json_file:
         json = f'{wine_data}'
-        out.write(json)
-
+        full_json_file.write(json)
     return wine_data
 
 
@@ -97,73 +94,74 @@ def calculate_avarage_price(varieties, wine_data):
     :param wine_data: data of each wine
     :return: avarage price
     """
-    price = [int(x['price']) for x in wine_data if x['variety'] == varieties and int(x['price']) > 0]
-    if len(price) == 0:
-        print(f'\tAvarage price for {varieties} is: ', 0)
+    list_price = [int(x['price']) for x in wine_data if x['variety'] == varieties and int(x['price']) > 0]
+    # if not len(list_price):
+    if not list_price:
+        print(f'\tAvarage price for {varieties} is: ',0)
         return 0
-    avarage_price = round(sum(price)/len(price), 3)
 
-    print("avarage price", avarage_price)
-    return avarage_price
+    avarage_price = round(sum(list_price)/len(list_price), 3)
+    print(f'\tAvarage price for {varieties} is: ',avarage_price)
+    # return avarage_price
 
 
 def calculate_min_price(varieties, wine_data):
     """Calculation of the minimum price of wine
     :return: minimum price
     """
-    price = [int(x['price']) for x in wine_data if x['variety'] == varieties and int(x['price']) > 0]
-    if len(price) == 0:
+    list_price = [int(x['price']) for x in wine_data if x['variety'] == varieties and int(x['price']) > 0]
+    # if not len(list_price):
+    if not list_price:
         print(f'\tMinimum price for {varieties} is: ', 0)
         return 0
 
-    minimum_price = min(price)
-    print(f'\tMinimum price for {varieties} is: ', minimum_price)
+    minimum_price = min(list_price)
+    print(f'\tMinimum price for {varieties} is: ',minimum_price)
     return minimum_price
 
 
 def calculate_max_price(varieties, wine_data):
     """Calculation of the maximum price of wine
-    :return: max price
+    :return: maximum price
     """
-    price = [int(x['price']) for x in wine_data if x['variety'] == varieties and int(x['price']) > 0]
-    if len(price) == 0:
-        print(f'\tMaximum price for {varieties} is: ', 0)
+    list_price = [int(x['price']) for x in wine_data if x['variety'] == varieties and int(x['price']) > 0]
+    # if not len(list_price):
+    if not list_price:
+        print(f'\tMaximum price for {varieties} is: ',0)
         return 0
-    max_price = max(price)
-    print(f'\tMaximum price for {varieties} is: ', max_price)
+    max_price = max(list_price)
+    print(f'\tMaximum price for {varieties} is: ',max_price)
     return max_price
 
 
 def calculate_most_common_region(varieties, wine_data):
-    """Calculation of the place where most
+    """Calculation of the region where most
     wines of this variety are produced
+    :return: None
     """
     region_1 = [x['region_1'] for x in wine_data if x['variety'] == varieties and x['region_1'] != '0']
     region_2 = [x['region_2'] for x in wine_data if x['variety'] == varieties and x['region_2'] != '0']
     regions = region_1 + region_2
-    most_common_region = {}
-    keys = list(set(regions))
 
-    for region in keys:
-        # reg = region
-        kye = [region]
-        v = [regions.count(region)]
-        data_tmp = dict(zip(kye, v))
-        most_common_region.update(data_tmp)
+    if not regions:
+        print(f'\tMost common region for {varieties} is: ',0)
+        return 0
 
-    name = ''
-    count = 0
-    for key in most_common_region.keys():
-        if most_common_region[key] > count:
-            count = most_common_region[key]
-            name = key
+    region_counter = collections.defaultdict(int)
+    for i in regions:
+        region_counter[i] += 1
+    max_val = max(region_counter.values())
+    final_dict = {k: v for k, v in region_counter.items() if v == max_val}
+    name = [i for i in final_dict.keys()]
+    print(f'\tMost common region for {varieties} is: ',name[0], max_val)
 
-    print(f'\tMost common region for {varieties} is: ', name, count)
+
 
 
 def calculate_most_common_country(varieties, wine_data):
-    """
-    :return:
+    """Calculation of the country where most
+    wines of this variety are produced
+    :return: None
     """
     countries = [x['country'] for x in wine_data if x['variety'] == varieties and x['country'] != '0']
     most_common = {}
@@ -171,23 +169,43 @@ def calculate_most_common_country(varieties, wine_data):
     # print(countries)
     # print(keys)
     for country in keys:
-        r = country     # need to be assign as a string for first
+        r = country
         kye = [r]
         v = [countries.count(country)]
         tmp_d = dict(zip(kye, v))
         most_common.update(tmp_d)
 
-    count = 0
     name = ''
+    count = 0
     for key in most_common.keys():
         if most_common[key] > count:
             count = most_common[key]
             name = key
 
-    print(f'\tMost country region for {varieties} is: ', name, count)
+    print(f'\tMost common country for {varieties} is: ',name, count)
+
+    if not countries:
+        print(f'\tMost common country for {varieties} is: ',0)
+        return 0
+
+    countries_counter = collections.defaultdict(int)
+    for i in countries:
+        countries_counter[i] += 1
+    max_val = max(countries_counter.values())
+    final_dict = {k: v for k, v in countries_counter.items() if v == max_val}
+    name = [i for i in final_dict.keys()]
+    print(f'\tMost common country for {varieties} is: ',name[0], max_val)
+
+
 
 
 def calculate_avarage_score(varieties, wine_data):
+    """
+
+    :param varieties:
+    :param wine_data:
+    :return:
+    """
     points = [int(x['points']) for x in wine_data if x['variety'] == varieties and int(x['points']) > 0]
     if len(points) == 0:
         print(f'\tAvarage score for {varieties} is: ', 0)
@@ -198,12 +216,22 @@ def calculate_avarage_score(varieties, wine_data):
 
 
 def calculate_most_expensive_wine(wine_data):
+    """
+
+    :param wine_data:
+    :return:
+    """
     wine_data = sorted(wine_data, key=lambda x: int(x['price']), reverse=True)
     print(f'\tMost expensive wine is: ', wine_data[0]['variety'], wine_data[0]['price'])
     return (wine_data[0]['variety'], wine_data[0]['price'])
 
 
 def calculate_cheapest_wine(wine_data):
+    """
+
+    :param wine_data:
+    :return:
+    """
     cheapest = [(x['variety'], int(x['price'])) for x in wine_data if int(x['price']) > 0]
     cheapest_dict = dict(sorted(dict(cheapest).items(), key=lambda x: x[1]))
     _price = 0
@@ -237,10 +265,16 @@ if __name__ == '__main__':
 
     concat_text = join_jsons(json_file_1, json_file_2)
     keys, datas_betw_braces = extraction_data_between_braces(concat_text)
-    wine_data = get_wine_data(keys, datas_betw_braces)
+    wine_data_unsort = get_wine_data(keys, datas_betw_braces)
+    wine_data = sort_wine_data(wine_data_unsort)
 
     for varieties in varieties_of_wines:
+        print(f"Statistic for Varieties: {varieties}", end="\n"*2)
         calculate_avarage_price(varieties, wine_data)
         calculate_min_price(varieties, wine_data)
         calculate_max_price(varieties, wine_data)
         calculate_most_common_region(varieties, wine_data)
+
+        # calculate_most_common_country(varieties, wine_data)
+        # calculate_avarage_score(varieties, wine_data)
+        print("\n")
